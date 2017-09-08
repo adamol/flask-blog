3b6b9417ac2181a1f225e4a5e25098cf136cd24a
Adam Olsson <olsson@billiger-mietwagen.com>
2017-09-08 16:39:57 +0200

    Incorporate SQLAlchemy and setup initial registration/login

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from passlib.hash import sha256_crypt
 
 app = Flask(__name__)
-app.debug = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SECRET_KEY'] = 'random string'
+db = SQLAlchemy(app)
 
 posts = [
     {
@@ -15,35 +18,80 @@ posts = [
     }
 ]
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True)
+    email = db.Column(db.String(120), unique=True)
+    password = db.Column(db.String(255))
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+db.create_all()
+
 @app.route('/')
 def welcome():
     return render_template('welcome.html')
 
+"""
+AUTH ROUTES
+"""
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        encrypted_pass = sha256_crypt.encrypt(request.form['password'])
+        if request.form['password'] != request.form['confirm_password']:
+            return redirect(url_for('register'))
 
-        return jsonify({
-            'username': request.form['username'],
-            'email': request.form['email'],
-            'password': encrypted_pass,
-            'confirm_password': request.form['confirm_password'],
-            'check_pass': sha256_crypt.verify('secret', encrypted_pass)
-        })
+        if request.form['password'] == '' or request.form['username'] == '' or request.form['email'] == '':
+            return redirect(url_for('register'))
+
+        user = User(
+            request.form['username'],
+            request.form['email'],
+            sha256_crypt.encrypt(request.form['password'])
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        flash('User was added')
+        return redirect(url_for('posts_index'))
     else:
         return render_template('register.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    if request.method == 'POST':
+        user = User.query.filter_by(username=request.form['username']).first()
+        if not user:
+            flash('no user with that username')
 
-#
+            return redirect(url_for('login'))
 
+        if not sha256_crypt.verify(request.form['password'], user.password):
+            flash('login credentials did not match our records')
+
+            return redirect(url_for('login'))
+
+        flash('You are now logged in')
+
+        return jsonify({
+            'username': user.username,
+            'email': user.email,
+            'password': user.password,
+        })
+    else:
+        return render_template('login.html')
+
+"""
+POST ROUTES
+"""
 @app.route('/posts')
-def postsIndex():
+def posts_index():
     return jsonify(posts)
 
 @app.route('/posts/<int:id>')
-def postsShow(id):
+def posts_show(id):
     return jsonify(posts[id])
