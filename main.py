b3f9ae0f045833f11413e1f4d9b8377347852757
Adam Olsson <olsson@billiger-mietwagen.com>
2017-09-08 15:51:48 +0200

    Added passlib dependency and stubbed out first version of register endpoint

--- main.py
+++ main.py
@@ -1,6 +1,49 @@
-from flask import Flask
+from flask import Flask, jsonify, render_template, request
+from passlib.hash import sha256_crypt
+
 app = Flask(__name__)
+app.debug = True
+
+posts = [
+    {
+        'title': 'First Post',
+        'body': 'Lorem ipsum, dolar sit amet'
+    },
+    {
+        'title': 'Second Post',
+        'body': 'Some more body content'
+    }
+]
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def welcome():
+    return render_template('welcome.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        encrypted_pass = sha256_crypt.encrypt(request.form['password'])
+
+        return jsonify({
+            'username': request.form['username'],
+            'email': request.form['email'],
+            'password': encrypted_pass,
+            'confirm_password': request.form['confirm_password'],
+            'check_pass': sha256_crypt.verify('secret', encrypted_pass)
+        })
+    else:
+        return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
+
+#
+
+@app.route('/posts')
+def postsIndex():
+    return jsonify(posts)
+
+@app.route('/posts/<int:id>')
+def postsShow(id):
+    return jsonify(posts[id])
